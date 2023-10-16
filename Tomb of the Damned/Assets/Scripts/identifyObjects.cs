add11cb4c409650a8c9c3259b9b108f984317cd7
dtalmood <dtalmood1@gmail.com>
2023-10-15 23:38:10 -0700

    Can now Idenitfy Pickupable Objects

--- Tomb of the Damned/Assets/Scripts/identifyObjects.cs
+++ Tomb of the Damned/Assets/Scripts/identifyObjects.cs
@@ -7,18 +7,25 @@ public class identifyObjects : MonoBehaviour
     public Camera playerCamera;
     public float maxDistance = 10f;
 
+    public string objectName;
+    public string objectTag;
+
     void Update()
     {
         Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
         RaycastHit hit;
 
+        // if statement checks if we are looking at a game object
         if (Physics.Raycast(ray, out hit, maxDistance))
         {
             GameObject hitObject = hit.collider.gameObject;
-            string objectName = hitObject.name;
-            string objectTag = hitObject.tag;
-
+            objectName = hitObject.name;
+            objectTag = hitObject.tag;
             Debug.Log("Looking at: " + objectName);
+
+            // Send data to the script that handles picking up objects such as gold
+            pickingUpObjects.recieveObjectName = objectName;
+            pickingUpObjects.recieveObjectTag = objectTag;
         }
     }
 }
