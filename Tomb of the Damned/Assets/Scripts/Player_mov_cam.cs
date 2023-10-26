19c7f65c808e3688fb50ebd7f3ffd9705e4f2db9
Nick <njsandburg@gmail.com>
2023-10-25 20:00:34 -0700

    States of the player
    added states for walking, jumping, running, crouch is not working.

--- Tomb of the Damned/Assets/Scripts/Player_mov_cam.cs
+++ Tomb of the Damned/Assets/Scripts/Player_mov_cam.cs
@@ -6,16 +6,36 @@ using UnityEngine;
 public class FPSController : MonoBehaviour
 {
     public Camera playerCamera;
+    // walk, run, jump, grav
     public float walkSpeed = 6f;
     public float runSpeed = 12f;
     public float jumpPower = 7f;
     public float gravity = 10f;
- 
+
+    // crouching
+    bool isCrouching;
+    public float crouchSpeed = 3f;
+    public float crouchYscale;
+    private float startYScale;
+    public KeyCode crouchKey = KeyCode.LeftControl;
  
     public float lookSpeed = 2f;
     public float lookXLimit = 45f;
- 
- 
+
+    Rigidbody rb;
+
+    // stores current movement state
+    public MovementState state;
+
+    // movement state
+    public enum MovementState
+    {
+        walking,
+        running,
+        crouching,
+        air
+    }
+
     Vector3 moveDirection = Vector3.zero;
     float rotationX = 0;
  
@@ -28,11 +48,12 @@ public class FPSController : MonoBehaviour
         characterController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        // set start y scale
+        startYScale = transform.localScale.y;
     }
  
     void Update()
     {
- 
         #region Handles Movment
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
@@ -43,16 +64,49 @@ public class FPSController : MonoBehaviour
         float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
- 
+        // set state to run
+        if (isRunning)
+        {
+            state = MovementState.running;
+            Debug.Log("Player is running");
+        }
+
+        // crouching 
+        if (Input.GetKeyDown(crouchKey))
+        {
+            // change y
+            transform.localScale = new Vector3(transform.localScale.x, crouchYscale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+
+            // set crouch state
+            state = MovementState.crouching;
+            curSpeedX = crouchSpeed;
+            isCrouching = true;
+        }
+        
+        // stop crouching
+        if (Input.GetKeyUp(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+            isCrouching = false;
+            state = MovementState.walking;
+        }
+
         #endregion
  
         #region Handles Jumping
-        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        bool isJumping;
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded && !isCrouching)
         {
+            isJumping = true;
             moveDirection.y = jumpPower;
+            // set jump state
+            state = MovementState.air;
+            Debug.Log("Player is jumping");
         }
         else
         {
+            isJumping = false;
             moveDirection.y = movementDirectionY;
         }
  
@@ -60,6 +114,14 @@ public class FPSController : MonoBehaviour
         {
             moveDirection.y -= gravity * Time.deltaTime;
         }
+        
+        // set walk state
+        if (!isJumping && !isRunning && !isCrouching)
+        {
+            state = MovementState.walking;
+            Debug.Log("Player is walking");
+        }
+        
  
         #endregion
  
