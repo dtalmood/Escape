ae0f3c6c5a36f1b9f6184ea1e134e9bae7ef12ab
Nick <njsandburg@gmail.com>
2023-10-27 15:59:26 -0700

    Fixed gravity issues, player interactions
    Fixed gravity issues, player interaction with objects.

--- Tomb of the Damned/Assets/Scripts/PlayerMovement.cs
+++ Tomb of the Damned/Assets/Scripts/PlayerMovement.cs
@@ -37,7 +37,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponentInChildren<Rigidbody>();
         rb.freezeRotation = true;
 
         readyToJump = true;
