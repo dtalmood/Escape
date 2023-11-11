42b3f10982b787d074c92dc331b0b24a17249858
Nick <njsandburg@gmail.com>
2023-11-10 18:35:01 -0800

    Can add different items (back-end)
    We are able to add objects for the game but they do not pop up yet.

---
+++ Tomb of the Damned/Assets/Scriptable Objects/Inventory/Scripts/InventoryObject.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory System/Inventory")]
+public class InventoryObject : ScriptableObject
+{
+    public List<InventorySlot> Container = new List<InventorySlot>();
+    public void AddItem(ItemObject _item, int _amount)
+    {
+        // check if item is in inv
+        bool hasItem = false;
+        for (int i = 0; i < Container.Count; i++)
+        {
+            if (Container[i].item == _item)
+            {
+                Container[i].AddAmount(_amount);
+                hasItem = true; 
+                break;
+            }
+        }
+        // item is not in there so add it
+        if (!hasItem)
+        {
+            Container.Add(new InventorySlot(_item, _amount));
+        }
+    }
+}
+
+[System.Serializable]
+public class InventorySlot
+{
+    // item and amount of it
+    public ItemObject item;
+    public int amount;
+
+    // constructor
+    public InventorySlot(ItemObject _item, int _amount)
+    {
+        item= _item;
+        amount= _amount;
+    }
+
+    public void AddAmount(int value)
+    {
+        // add the value to the current amount
+        amount+= value;
+    }
+
+}
---
+++ Tomb of the Damned/Assets/Scriptable Objects/Items/Scripts/ItemObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    potions,
+    equipment,
+    Default
+}
+
+public abstract class ItemObject : ScriptableObject
+{
+    // holds display for item
+    public GameObject prefab;
+    // store type of item
+    public ItemType type;
+    [TextArea(15,20)]
+    // description of item
+    public string description;
+}
