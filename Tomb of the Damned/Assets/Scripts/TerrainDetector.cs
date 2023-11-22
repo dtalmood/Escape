5a8fb4358d29649d41c2cd57558eacb270498ef4
dtalmood <dtalmood1@gmail.com>
2023-11-21 18:13:59 -0800

    Terrain layers can now be distinguished

--- Tomb of the Damned/Assets/Scripts/TerrainDetector.cs
+++ Tomb of the Damned/Assets/Scripts/TerrainDetector.cs
@@ -12,17 +12,18 @@ public class TerrainDetector : MonoBehaviour
 {
     // Reference to the terrain
     public Terrain terrain;
+    
 
     void Update()
     {
-        testingFunction();
-        detectTerrain();
-        
-        
+        //testingFunction();
+        //detectTerrain();
+        getTextureMix();
     }
 
     private void testingFunction()
     {
+        //Debug.Log(transform.position);
         // Debug.Log(Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, Mathf.Infinity, LayerMask.GetMask("Terrain")));
         // Debug.Log("Layer Mask: "+LayerMask.GetMask("whatIsGround"));
         // Debug.Log("Vector3 Down =  "+ Vector3.down);
@@ -44,7 +45,7 @@ public class TerrainDetector : MonoBehaviour
             // Get the terrain texture coordinates
             Vector3 terrainCoords = hit.textureCoord;
             //Debug.Log("hit.point = "+hit.point);
-            Debug.Log("hit.point = "+hit.textureCoord);
+            //Debug.Log("hit.point = "+hit.textureCoord);
             
             // Get the alpha map values at the coordinates
             float[,,] alphaMapData = terrain.terrainData.GetAlphamaps(
@@ -57,18 +58,38 @@ public class TerrainDetector : MonoBehaviour
             int dominantTextureIndex = 0;
             float maxAlpha = 0f;
 
-            for (int i = 0; i < alphaMapData.GetLength(2); i++)
+            for (int i = 0; i < alphaMapData.GetLength(1); i++)
             {
+                
                 if (alphaMapData[0, 0, i] > maxAlpha)
                 {
                     maxAlpha = alphaMapData[0, 0, i];
                     dominantTextureIndex = i;
                 }
             }
-        
-            // Print the dominant terrain type
-            //Debug.Log("Running");
-            //Debug.Log("Player is on terrain type: " + terrain.terrainData.terrainLayers[dominantTextureIndex].name);
+            //Debug.Log("Alpha Map Data Sand: "+alphaMapData[0, 0, 0]);
+            //Debug.Log("Alpha Map Data BS: "+alphaMapData[0, 0, 1]);
+
+            
+            Debug.Log("Player is on terrain type: " + terrain.terrainData.terrainLayers[dominantTextureIndex].name);
         }
+    }
+
+    private void getTextureMix()
+    {
+        Vector3 tpos = terrain.transform.position;
+        TerrainData tData = terrain.terrainData;
+        int mapX = Mathf.RoundToInt((transform.position.x - tpos.x) / tData.size.x * tData.alphamapWidth);
+        int mapZ = Mathf.RoundToInt((transform.position.z - tpos.z) / tData.size.z * tData.alphamapHeight);
+        float[,,] splatMapData = tData.GetAlphamaps(mapX, mapZ, 1, 1);
+
+        float[] cellmix = new float[splatMapData.GetUpperBound(2)+1];
+        for(int i = 0; i < cellmix.Length; i++)
+        {
+            cellmix[i] = splatMapData[0,0,i];
+        }
+        // Use the mapX variable as needed
+        Debug.Log("cellmix[1] = " + cellmix[0]);
+        Debug.Log("cellmix[2] = " + cellmix[1]);
     }
 }
